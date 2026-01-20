7c55d65b14cc7f97cec00845bb742af3df72ecbf
Nico Ritschel <ritschel@gmail.com>
2026-01-20 10:43:50 -0800

    pynb: 0.1.0 (1)

--- Casks/pynb.rb
+++ Casks/pynb.rb
@@ -1,7 +1,7 @@
 cask "pynb" do
-  version "1.0,1"
-  sha256 "cc977bea6ab562957814dfc33551f676f2ad3b41098790a0148d4379e1080637"
-  asset_id = 343397623
+  version "0.1.0,1"
+  sha256 "94a93cd8d6fb46e63cd777d7bb133cf421deb0d90bb2d70d2960e0f91fa9addf"
+  asset_id = 343401793
 
   token = ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]
 
@@ -14,7 +14,7 @@ cask "pynb" do
           "X-GitHub-Api-Version: 2022-11-28",
         ]
   else
-    url "https://github.com/sidequery/homebrew-tap/releases/download/pynb-1.0-1/pynb-1.0-1-macos14.0-arm64.zip",
+    url "https://github.com/sidequery/homebrew-tap/releases/download/pynb-0.1.0-1/pynb-0.1.0-1-macos14.0-arm64.zip",
         verified: "github.com/sidequery/homebrew-tap/"
   end
 
