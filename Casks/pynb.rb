99426ce211e3782269ee4f32358cdc718fcf62e9
Nico Ritschel <ritschel@gmail.com>
2026-01-20 10:30:41 -0800

    pynb: 1.0 (1)

--- Casks/pynb.rb
+++ Casks/pynb.rb
@@ -1,18 +1,22 @@
 cask "pynb" do
   version "1.0,1"
   sha256 "cc977bea6ab562957814dfc33551f676f2ad3b41098790a0148d4379e1080637"
-  asset_id RA_kwDOQ9uY-c4Ud9Kz
+  asset_id 343397623
 
   token = ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]
-  raise "Set HOMEBREW_GITHUB_API_TOKEN (or GITHUB_TOKEN) to download this private cask" unless token
 
-  url "https://api.github.com/repos/sidequery/homebrew-tap/releases/assets/#{asset_id}",
-      verified: "github.com/sidequery/homebrew-tap/",
-      header: [
-        "Accept: application/octet-stream",
-        "Authorization: Bearer #{token}",
-        "X-GitHub-Api-Version: 2022-11-28",
-      ]
+  if token
+    url "https://api.github.com/repos/sidequery/homebrew-tap/releases/assets/#{asset_id}",
+        verified: "github.com/sidequery/homebrew-tap/",
+        header: [
+          "Accept: application/octet-stream",
+          "Authorization: Bearer #{token}",
+          "X-GitHub-Api-Version: 2022-11-28",
+        ]
+  else
+    url "https://github.com/sidequery/homebrew-tap/releases/download/pynb-1.0-1/pynb-1.0-1-macos14.0-arm64.zip",
+        verified: "github.com/sidequery/homebrew-tap/"
+  end
 
   name "pynb"
   desc "Minimal notebook app"
