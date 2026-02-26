f2d677605b2bc300d6645bfed3ed34f15639002d
Nico Ritschel <ritschel@gmail.com>
2026-02-26 09:12:50 -0800

    mdnb: 0.5.0 (20)

--- Casks/mdnb.rb
+++ Casks/mdnb.rb
@@ -1,7 +1,7 @@
 cask "mdnb" do
-  version "0.4.2,19"
-  sha256 "bd2a0348780a496fe2b2d7d697c4a677f553ff719ae5d7a8e2839b072bd24c37"
-  asset_id = 362946156
+  version "0.5.0,20"
+  sha256 "a120963adaca503a96afb3ff816fe1fe479d2b527a87604fa128546b96308977"
+  asset_id = 362952594
 
   token = ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]
 
@@ -14,7 +14,7 @@ cask "mdnb" do
           "X-GitHub-Api-Version: 2022-11-28",
         ]
   else
-    url "https://github.com/sidequery/homebrew-tap/releases/download/mdnb-0.4.2-19/mdnb-0.4.2-19-macos14.0-arm64.zip",
+    url "https://github.com/sidequery/homebrew-tap/releases/download/mdnb-0.5.0-20/mdnb-0.5.0-20-macos14.0-arm64.zip",
         verified: "github.com/sidequery/homebrew-tap/"
   end
 
